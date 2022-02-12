d823192bf20d4a17132c6c2724ad86bb372cb427
Alex Power[s] BI <44716363+itsnotaboutthecell@users.noreply.github.com>
2022-02-12 10:39:06 -0600

    Create PowerBI_Delete_Orphan_WS.ps1

---
+++ notebooks/PowerBI_Delete_Orphan_WS.ps1
@@ -0,0 +1,11 @@
+Connect-PowerBIServiceAccount
+
+$ws = Get-PowerBIWorkspace -Scope Organization -Orphaned
+
+write-host "Total orphaned workspaces: $($ws.count)`n"
+
+ForEach ($orphan in $ws)
+{
+    write-host $oprhan.Id
+    Invoke-PowerBIRestMethod -Url 'https://api.powerbi.com/v1.0/myorg/groups/$($orphan.id)' -Method Delete
+}
