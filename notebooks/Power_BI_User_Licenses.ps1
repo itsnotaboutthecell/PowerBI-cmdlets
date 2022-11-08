0b0a13050e8df05556ac31fbd33b005f86d700c0
Alex Power[s] BI <44716363+itsnotaboutthecell@users.noreply.github.com>
2022-11-07 19:55:53 -0600

    Tons of updates. Graph search.
    Updates for Tommy.

--- notebooks/Power_BI_User_Licenses.ps1
+++ notebooks/Power_BI_User_Licenses.ps1
@@ -3,29 +3,39 @@ $outPath = "C:\temp"
 #########################################
 
 # Azure AD module: https://learn.microsoft.com/powershell/module/azuread/?view=azureadps-2.0
-$m = "AzureAD"
+$m = "Microsoft.Graph"
 
-try {Import-Module $m -ErrorAction Stop} 
-catch {Install-Module -Name $m -Scope CurrentUser -Force} 
-finally {Import-Module $m}
-
-Connect-AzureAD
+try { Import-Module $m -ErrorAction Stop } 
+catch { Install-Module -Name $m -Scope CurrentUser -Force } 
+finally { Import-Module $m }
 
 if ($outPath.Substring($outPath.Length - 1, 1) -cne "\") { $outPath = $outPath + "\" }
 if (!(Test-Path $outPath)) { New-Item -ItemType Directory -Force -Path $outPath }
 
+Connect-Graph -Scopes Organization.Read.All
+
 # Power BI Licensing Types and Capabilities Official Docs: https://docs.microsoft.com/power-bi/admin/service-admin-licensing-organization#license-types-and-capabilities
 # For a complete listing of all Licensing Service Plans visit: https://docs.microsoft.com/azure/active-directory/enterprise-users/licensing-service-plan-reference
 
-$licenseType = "POWER_BI_STANDARD","POWER_BI_ADDON","PBI_PREMIUM_P1_ADDON","PBI_PREMIUM_PER_USER","PBI_PREMIUM_PER_USER_ADDON","PBI_PREMIUM_PER_USER_DEPT","POWER_BI_PRO","POWER_BI_PRO_CE","POWER_BI_PRO_DEPT"
+$pbiSubscriptions = Get-MgSubscribedSku | Select -Property Sku*, ConsumedUnits -ExpandProperty PrepaidUnits | Where-Object { ($_.SkuPartNumber.contains("POWER_BI") -or $_.SkuPartNumber.contains("PBI")) }
+$pbiSubscriptions | Format-List
 
-$allUsers = Get-AzureADUser -All | where {$_.isLicensed -eq "True"}
+$licenseType = $pbiSubscriptions | Select SkuPartNumber
+
+# Graph licensing search: https://learn.microsoft.com/microsoft-365/enterprise/view-licensed-and-unlicensed-users-with-microsoft-365-powershell?view=o365-worldwide
 
 foreach ($license in $licenseType) {
 
-    $licenses = $allUsers | Where-Object {($_.licenses).AccountSkuId -match ($license)}
+    $licenseName = $license.SkuPartNumber
+    Write-Host "Now Searching: $($licenseName)"
+    $licenses = Get-MgSubscribedSku -All | Where SkuPartNumber -eq $licenseName
+    $licenses | Select -Property Id, Sku*, CapabilityStatus, ConsumedUnits
 
-    Write-Host "Now Exporting Report: $($license)"
-    $licenses | Export-Csv -Path "$($outPath)$($license)_$(Get-Date -Format "yyyyMMdd").csv" -NoTypeInformation
+    if ($licenses.ConsumedUnits -ne 0) {
+
+        Write-Host "Now Exporting Report: $($licenseName)`n"
+        $licenses | Export-Csv -Path "$($outPath)$($licenseName)_$(Get-Date -Format "yyyyMMdd").csv" -NoTypeInformation
+
+    }
 
 }
