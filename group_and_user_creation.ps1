f40a289429de44ec6dd102a5489d07e92a0278d1
Alex Power[s] BI <44716363+itsnotaboutthecell@users.noreply.github.com>
2025-03-24 08:30:39 -0500

    Update group_and_user_creation.ps1

--- group_and_user_creation.ps1
+++ group_and_user_creation.ps1
@@ -1,17 +1,17 @@
 # Script variables
 $domain = "test.onmicrosoft.com" # Domain address - ex. example.onmicrosoft.com
-$owner = '' # Object ID of security group owner
-$experiences = @('') # An array of user groups to be created
+$owners = @('') # Object ID of security group owners
+$experiences = @('') # An array of user group names to be created
 $location = "US" # Geo location
 $license = "a403ebcc-fae0-4ca2-8c8c-7a907fd6c235" # POWER_BI_STANDARD
 
 $PasswordProfile = @{
-    Password                      = "" # Password
+    Password                      = "FabCon2025" # Password
     ForceChangePasswordNextSignIn = $true
 }
 
 # Function to check and install module if not present
-function Ensure-Module {
+function Install-ModuleIfNeeded {
     param (
         [string]$ModuleName
     )
@@ -20,13 +20,8 @@ function Ensure-Module {
     }
 }
 
-$modules = @("Microsoft.Graph.Users", "Microsoft.Graph.Users.Actions", "Microsoft.Graph.Authentication", "Microsoft.Graph.Groups")
-foreach ($module in $modules) { Ensure-Module -ModuleName $module }
-
-# Connect to Microsoft Graph
-Connect-MgGraph -Scopes User.ReadWrite.All, Group.ReadWrite.All -UseDeviceAuthentication
-
-function Create-UserProfile {
+# Function to create a user profile
+function New-UserProfile {
     param (
         [string]$experience,
         [int]$userIndex
@@ -39,6 +34,84 @@ function Create-UserProfile {
     }
 }
 
+# Function to add group owner
+function Add-GroupOwner {
+    param(
+        [string]$groupId,
+        [string]$owner
+    )
+
+    New-MgGroupOwner -GroupId $groupId -DirectoryObjectId $owner
+}
+
+# Function to create a new security group
+function New-SecurityGroup {
+    param (
+        [string]$groupName,
+        [string]$mailNickName,
+        [array]$owners
+    )
+
+    # Check if the group already exists
+    $existingGroup = Get-MgGroup -Filter "displayName eq '$($groupName)'" -ErrorAction SilentlyContinue
+
+    if ($existingGroup) {
+        Write-Output "'$($groupName)' already exists. No action taken."
+        return $existingGroup.Id
+    }
+    else {
+        # Create a new security group
+        $group = New-MgGroup -DisplayName $groupName `
+            -MailEnabled:$false `
+            -MailNickName $mailNickName `
+            -SecurityEnabled
+
+        $groupId = $group.Id
+
+        # Loop through each owner ID and add them to the group
+        foreach ($owner in $owners) {
+            Add-GroupOwner -groupId $groupId -owner $owner
+        }
+
+        return $groupId
+    }
+}
+
+# Function to create users and add them to a group
+function New-UsersAndAddToGroup {
+    param (
+        [string]$experience,
+        [int]$numberOfUsers,
+        [string]$groupId,
+        [int]$startIndex = 0
+    )
+
+    for ($currentCount = 0; $currentCount -lt $numberOfUsers; $currentCount++) {
+        
+        $userIndex = $startIndex + $currentCount
+
+        $userProfile = Create-UserProfile -experience $experience -userIndex $userIndex
+
+        # Create the user
+        $user = New-MgUser -UserPrincipalName $userProfile.Upn `
+            -PasswordProfile $PasswordProfile `
+            -DisplayName $userProfile.DisplayName `
+            -MailNickName $userProfile.MailNickName `
+            -AccountEnabled:$true -UsageLocation $location
+
+        Set-MgUserLicense -UserId $user.Id -AddLicenses @{SkuId = $license } -RemoveLicenses @()
+
+        New-MgGroupMember -GroupId $groupId -DirectoryObjectId $user.Id
+    }
+}
+
+# Ensure required modules are installed
+$modules = @("Microsoft.Graph.Users", "Microsoft.Graph.Users.Actions", "Microsoft.Graph.Authentication", "Microsoft.Graph.Groups")
+foreach ($module in $modules) { Ensure-Module -ModuleName $module }
+
+# Connect to Microsoft Graph
+Connect-MgGraph -Scopes User.ReadWrite.All, Group.ReadWrite.All -UseDeviceAuthentication
+
 foreach ($experience in $experiences) {
 
     $totalUsers = Read-Host -Prompt "Total number of users to create for $($experience)?"
@@ -50,77 +123,32 @@ foreach ($experience in $experiences) {
     if ($numberOfGroups -eq 1) {
 
         $groupName = "$($experience) Capacity Group"
-        $mailNickName = "$($experience)SecurityGroup0"
+        $mailNickName = "$($experience)SecurityGroup"
 
-        # Check if the group already exists
-        $existingGroup = Get-MgGroup -Filter "displayName eq '$($groupName)'" -ErrorAction SilentlyContinue
+        # Create a new security group and get its ID
+        $groupId = Create-SecurityGroup -groupName $groupName -mailNickName $mailNickName -owners $owners
 
-        if ($existingGroup) {
-            Write-Output "'$($groupName)' already exists. No action taken."
-        } else {
-            # Create a new security group
-            $group = New-MgGroup -DisplayName $groupName `
-                                 -MailEnabled:$false ` 
-                                 -MailNickName $mailNickName `
-                                 -SecurityEnabled
+        # Create users and add them to the group
+        New-UsersAndAddToGroup -experience $experience -numberOfUsers $numberOfUsers -groupId $groupId
 
-            $groupId = $group.Id
-
-            # Assign ownership to the Group
-            New-MgGroupOwner -GroupId $groupId -DirectoryObjectId $owner
-
-        }
-
-        for ($currentCount = 0; $currentCount -lt $numberOfUsers; $currentCount++) {
-            
-            $userProfile = Create-UserProfile -experience $experience -userIndex $currentCount
-
-            # Create the user
-            $user = New-MgUser -UserPrincipalName $userProfile.Upn -PasswordProfile $PasswordProfile -DisplayName $userProfile.DisplayName -MailNickName $userProfile.MailNickName -AccountEnabled:$true -UsageLocation $location
-            Set-MgUserLicense -UserId $user.Id -AddLicenses @{SkuId = $license } -RemoveLicenses @()
-
-            New-MgGroupMember -GroupId $groupId -DirectoryObjectId $user.Id
-        }
-    } else {
+    }
+    else {
 
         $usersPerGroup = [math]::Ceiling($numberOfUsers / $numberOfGroups)
 
         for ($groupCount = 0; $groupCount -lt $numberOfGroups; $groupCount++) {
 
             $groupName = "$($experience) Capacity Group $($groupCount)"
+            $mailNickName = "$($experience)SecurityGroup$($groupCount)"
 
-            # Check if the group already exists
-            $existingGroup = Get-MgGroup -Filter "displayName eq '$($groupName)'" -ErrorAction SilentlyContinue
+            # Create a new security group and get its ID
+            $groupId = Create-SecurityGroup -groupName $groupName -mailNickName $mailNickName -owners $owners
 
-            if ($existingGroup) {
-                Write-Output "'$($groupName)' already exists. No action taken."
-            } else {
-                # Create a new security group
-                $group = New-MgGroup -DisplayName $groupName `
-                                     -MailEnabled:$false `
-                                     -MailNickName "$($experience)SecurityGroup$($groupCount)" `
-                                     -SecurityEnabled
-
-                $groupId = $group.Id
-
-                # Assign ownership to the Group
-                New-MgGroupOwner -GroupId $groupId -DirectoryObjectId $owner
-            }
-
-            for ($currentCount = 0; $currentCount -lt $usersPerGroup; $currentCount++) {
-
-                $userIndex = $groupCount * $usersPerGroup + $currentCount
-
-                if ($userIndex -ge $numberOfUsers) { break }
-
-                $userProfile = Create-UserProfile -experience $experience -userIndex $userIndex
-
-                # Create the user
-                $user = New-MgUser -UserPrincipalName $userProfile.Upn -PasswordProfile $PasswordProfile -DisplayName $userProfile.DisplayName -MailNickName $userProfile.MailNickName -AccountEnabled:$true -UsageLocation $location
-                Set-MgUserLicense -UserId $user.Id -AddLicenses @{SkuId = $license } -RemoveLicenses @()
-
-                New-MgGroupMember -GroupId $groupId -DirectoryObjectId $user.Id
-            }
+            # Create users and add them to the group with appropriate start index
+            New-UsersAndAddToGroup  -experience $experience `
+                -numberOfUsers ([math]::Min($usersPerGroup, ($numberOfUsers - ($groupCount * $usersPerGroup)))) `
+                -groupId $groupId `
+                -startIndex ($groupCount * $usersPerGroup)
         }
     }
 }
